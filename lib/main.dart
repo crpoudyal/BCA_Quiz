dafaa422e4afa11d670418157762973559b82c8b
crpoudyal <crpoudyal@gmail.com>
2022-01-16 15:29:01 +0545

    firebase setup in android and web

--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,24 @@ import 'package:bca_quiz/responsive/responsive_layout_screen.dart';
 import 'package:bca_quiz/responsive/web_screen_layout.dart';
 import 'package:bca_quiz/utils/colors.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+      options: const FirebaseOptions(
+        apiKey: "AIzaSyDBrrOxUMkiKGD0U42MM0jimWxmaMFJyXg",
+        appId: "1:644571177544:web:fce07418152fa904266e7d",
+        messagingSenderId: "644571177544",
+        projectId: "bca-quiz-d0361",
+        storageBucket: "bca-quiz-d0361.appspot.com",
+      ),
+    );
+  } else {
+    await Firebase.initializeApp();
+  }
   runApp(const MyApp());
 }
 
