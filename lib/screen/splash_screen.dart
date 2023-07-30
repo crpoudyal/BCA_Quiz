0ba8bb5260201430da72b27a2a75b07a191083aa
crpoudyal <crpoudyal@gmail.com>
2023-07-30 21:58:53 +0545

    splash screen updated

--- lib/screen/splash_screen.dart
+++ lib/screen/splash_screen.dart
@@ -17,7 +17,7 @@ class _SplashScreenState extends State<SplashScreen> {
 
     Future.delayed(const Duration(seconds: 5)).then((value) {
       Navigator.pushReplacement(
-          context, MaterialPageRoute(builder: (context) => const HomeScreen()));
+          context, MaterialPageRoute(builder: (context) => const MobileScreenLayout()));
     });
   }
 
