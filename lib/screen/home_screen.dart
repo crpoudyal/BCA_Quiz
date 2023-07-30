117951482718292c0a6f35890080a5d65bd6e4f3
crpoudyal <crpoudyal@gmail.com>
2023-07-30 22:21:58 +0545

    updated home screen

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,89 +1,33 @@
-import 'dart:developer';
-
 import 'package:bca_quiz/screen/course_screen.dart';
+import 'package:bca_quiz/screen/quiz_home_screen.dart';
 import 'package:bca_quiz/utils/colors.dart';
 import 'package:bca_quiz/widgets/sem_container.dart';
 import 'package:flutter/material.dart';
-import 'package:html/parser.dart';
-import 'package:http/http.dart' as http;
-import 'package:url_launcher/url_launcher_string.dart';
 
 class HomeScreen extends StatefulWidget {
-  const HomeScreen({Key? key}) : super(key: key);
+  const HomeScreen({Key? key, this.onTap}) : super(key: key);
+  final Function? onTap;
 
   @override
   _HomeScreenState createState() => _HomeScreenState();
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  List<String> titles = [];
-  List<String> dates = [];
-  List<String> dlinks = [];
-  String sid = "";
-
-  @override
-  void initState() {
-    super.initState();
-    getDataFromWeb();
-  }
-
-  Future getDataFromWeb() async {
-    final url = Uri.parse('https://www.tufohss.edu.np/index.php/notices/');
-    final response = await http.get(url);
-    final body = response.body;
-    final html = parse(body);
-    final titles = html
-        .querySelectorAll(' tbody > tr:not(:first-child) > td:first-child')
-        .map((element) => element.innerHtml.trim())
-        .toList();
-
-    final dates = html
-        .querySelectorAll('tbody>tr:not(:first-child)>td:nth-child(1)')
-        .map((e) => e.innerHtml.trim())
-        .toList();
-    final dlinks = html
-        .querySelectorAll('tbody>tr:not(:first-child)>td:nth-child(2)>a')
-        .map((e) => e.attributes['href'].toString())
-        .toList();
-
-    log(dlinks.toString());
-    setState(() {
-      this.titles = titles;
-      this.dates = dates;
-      this.dlinks = dlinks;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: mobileBackgroundColor,
-        title: Row(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            Image.asset(
-              'assets/images/BCAquiz.png',
-              height: 64,
-            ),
-            const SizedBox(
-              width: 92,
-            ),
-            const Text("BCA Quiz"),
-          ],
+        title: const Text("BCA Hub"),
+        leading: Image.asset(
+          'assets/images/BCAquiz.png',
         ),
-        centerTitle: false,
-        actions: [
-          IconButton(
-            onPressed: () {},
-            icon: const Icon(Icons.notifications),
-          ),
-        ],
+        centerTitle: true,
       ),
       body: Column(
         children: [
           Container(
-            height: 140,
+            height: 150,
             color: mobileBackgroundColor,
             child: Padding(
               padding: const EdgeInsets.only(left: 15, right: 15),
@@ -100,16 +44,16 @@ class _HomeScreenState extends State<HomeScreen> {
                   const SizedBox(
                     height: 10,
                   ),
-                  Row(
+                  const Row(
                     mainAxisAlignment: MainAxisAlignment.center,
-                    children: const [
+                    children: [
                       Icon(
                         Icons.check_box,
                         size: 12,
                         color: Color(0xFF50DDF6),
                       ),
                       Text(
-                        " Quizes at any time anywhere",
+                        " Quiz's at any time anywhere",
                         style: TextStyle(color: Colors.white, fontSize: 12),
                       ),
                     ],
@@ -117,9 +61,9 @@ class _HomeScreenState extends State<HomeScreen> {
                   const SizedBox(
                     height: 5,
                   ),
-                  Row(
+                  const Row(
                     mainAxisAlignment: MainAxisAlignment.center,
-                    children: const [
+                    children: [
                       Icon(
                         Icons.check_box,
                         size: 12,
@@ -135,7 +79,16 @@ class _HomeScreenState extends State<HomeScreen> {
                     height: 8,
                   ),
                   TextButton(
-                    onPressed: () {},
+                    onPressed: () {
+                      Navigator.pushReplacement(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const QuizHomeScreen()));
+                    },
+                    style: TextButton.styleFrom(
+                      foregroundColor: Colors.white,
+                      backgroundColor: Colors.blue,
+                    ),
                     child: const Padding(
                       padding: EdgeInsets.fromLTRB(8, 1, 8, 1),
                       child: Text(
@@ -143,11 +96,7 @@ class _HomeScreenState extends State<HomeScreen> {
                         style: TextStyle(fontWeight: FontWeight.bold),
                       ),
                     ),
-                    style: TextButton.styleFrom(
-                      primary: Colors.white,
-                      backgroundColor: Colors.blue,
-                    ),
-                  )
+                  ),
                 ],
               ),
             ),
@@ -160,75 +109,84 @@ class _HomeScreenState extends State<HomeScreen> {
             style: TextStyle(
                 fontSize: 16, color: Colors.black, fontWeight: FontWeight.bold),
           ),
-          Padding(
-            padding: const EdgeInsets.all(10.0),
-            child: GridView(
-              shrinkWrap: true,
-              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 4),
-              children: [
-                GestureDetector(
-                    onTap: () {
-                      CourseScreen(sid: sid);
-                    },
-                    child:
-                        semContainer("First", Colors.blueAccent, Icons.code)),
-                semContainer("Second", Colors.redAccent, Icons.code),
-                semContainer("Third", Colors.greenAccent, Icons.code),
-                semContainer("Fourth", Colors.blueGrey, Icons.code),
-                semContainer("Fifth", Colors.purpleAccent, Icons.code),
-                semContainer("Sixth", Colors.cyan, Icons.code),
-                semContainer("Seventh", Colors.deepOrangeAccent, Icons.code),
-                semContainer("Eighth", Colors.brown, Icons.code),
-              ],
-            ),
-          ),
-          const SizedBox(
-            height: 8,
-          ),
-          const Text(
-            "Notices",
-            style: TextStyle(
-                fontSize: 16, color: Colors.black, fontWeight: FontWeight.bold),
-          ),
           Expanded(
-            child: SizedBox(
-              child: ListView.builder(
-                itemCount: titles.length,
-                itemBuilder: (context, index) {
-                  final title = titles[index];
-
-                  final date = dates[index];
-                  final dlink = dlinks[index];
-                  return ListTile(
-                    title: Text(
-                      title,
-                      style: const TextStyle(
-                          fontSize: 16,
-                          color: Colors.black,
-                          fontWeight: FontWeight.bold),
-                    ),
-                    subtitle: Text(
-                      date,
-                      style: const TextStyle(
-                        fontSize: 16,
-                        color: Colors.black,
-                      ),
-                    ),
-                    trailing: IconButton(
-                      icon: const Icon(Icons.remove_red_eye_rounded),
-                      color: mobileBackgroundColor,
-                      onPressed: () async {
-                        if (await canLaunchUrlString(dlink)) {
-                          await launchUrlString(dlink);
-                        }
-                      },
-                    ),
-                  );
-                },
+            child: Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: GridView(
+                shrinkWrap: true,
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 4,
+                ),
+                children: [
+                  semContainer("First", Colors.blueAccent, Icons.code, () {
+                    Navigator.pushReplacement(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const CourseScreen(
+                                  sid: '',
+                                )));
+                  }),
+                  semContainer("Second", Colors.redAccent, Icons.code, () {
+                    Navigator.pushReplacement(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const CourseScreen(
+                                  sid: '',
+                                )));
+                  }),
+                  semContainer("Third", Colors.greenAccent, Icons.code, () {
+                    Navigator.pushReplacement(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const CourseScreen(
+                                  sid: '',
+                                )));
+                  }),
+                  semContainer("Fourth", Colors.blueGrey, Icons.code, () {
+                    Navigator.pushReplacement(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const CourseScreen(
+                                  sid: '',
+                                )));
+                  }),
+                  semContainer("Fifth", Colors.purpleAccent, Icons.code, () {
+                    Navigator.pushReplacement(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const CourseScreen(
+                                  sid: '',
+                                )));
+                  }),
+                  semContainer("Sixth", Colors.cyan, Icons.code, () {
+                    Navigator.pushReplacement(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const CourseScreen(
+                                  sid: '',
+                                )));
+                  }),
+                  semContainer("Seventh", Colors.deepOrangeAccent, Icons.code,
+                      () {
+                    Navigator.pushReplacement(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const CourseScreen(
+                                  sid: '',
+                                )));
+                  }),
+                  semContainer("Eighth", Colors.brown, Icons.code, () {
+                    Navigator.pushReplacement(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const CourseScreen(
+                                  sid: '',
+                                )));
+                  }),
+                ],
               ),
             ),
-          ),
+          )
         ],
       ),
     );
