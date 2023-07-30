c2cfc1f42a9a19e0a2d91f89a5f2e97cf84dc25a
crpoudyal <crpoudyal@gmail.com>
2023-07-30 22:32:07 +0545

    upadate score

--- lib/screen/result_screen.dart
+++ lib/screen/result_screen.dart
@@ -1,7 +1,11 @@
 import 'package:bca_quiz/models/question.dart';
 import 'package:bca_quiz/screen/quiz_screen.dart';
 import 'package:bca_quiz/utils/colors.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+
+import '../responsive/mobile_screen_layout.dart';
 
 class ResultScreen extends StatefulWidget {
   const ResultScreen({
@@ -20,6 +24,12 @@ class ResultScreen extends StatefulWidget {
 }
 
 class _ResultScreenState extends State<ResultScreen> {
+  @override
+  void initState() {
+    super.initState();
+    _updateScore();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -50,9 +60,43 @@ class _ResultScreenState extends State<ResultScreen> {
               child: const Text("Play Again"),
             ),
             const SizedBox(height: 40),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).pushReplacement(
+                  MaterialPageRoute(
+                    builder: (context) => const MobileScreenLayout(),
+                  ),
+                );
+              },
+              child: const Text("Go to Home"),
+            )
           ],
         ),
       ),
     );
+  }
+
+  Future<void> _updateScore() async {
+    final authUser = FirebaseAuth.instance.currentUser;
+    if (authUser != null) return;
+    final userRef =
+        FirebaseFirestore.instance.collection('users').doc(authUser!.uid);
+    final userDoc = await userRef.get();
+
+    if (userDoc.exists) {
+      final user = userDoc.data()!;
+
+      final lastScore = user['score'].get();
+
+      userRef.update({'score': lastScore + widget.score});
+
+      return;
+    }
+    userRef.set({
+      'username': authUser.displayName,
+      'email': authUser.email,
+      'photoUrl': authUser.photoURL,
+      'score': widget.score
+    });
   }
 }
